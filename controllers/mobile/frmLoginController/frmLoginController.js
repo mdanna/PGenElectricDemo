96e16628e5e5d28a9e631d8182b57a918c328e44
Mario D'Anna <mario.danna@hcl.com>
2022-02-25 10:37:39 +0100

    Completed frmLogin

--- controllers/mobile/frmLoginController/frmLoginController.js
+++ controllers/mobile/frmLoginController/frmLoginController.js
@@ -1,5 +1,19 @@
 define({ 
 
- //Type your controller code here 
+	onViewCreated(){
+      this.view.init = () => {
+        this.view.lblRegisterNow.onTouchEnd = () => {
+          new voltmx.mvc.Navigation("frmSaveTimeAndMoney").navigate();
+        };
+        
+        this.view.flxCheckbox.onClick = () => {
+          this.view.lblCheck.isVisible = !this.view.lblCheck.isVisible;
+        };
+        
+        this.view.flxBtnLogin.onClick = () => {
+          new voltmx.mvc.Navigation("frmMap").navigate();
+        };
+      };
+    }
 
- });
+});
