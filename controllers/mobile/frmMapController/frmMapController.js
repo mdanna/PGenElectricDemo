371cd2552bd37e7966a8b0e7d74c8c1ee6c47cdd
Mario D'Anna <mario.danna@hcl.com>
2022-02-25 16:59:37 +0100

    Finished frmMap UI with hamburger menu

--- controllers/mobile/frmMapController/frmMapController.js
+++ controllers/mobile/frmMapController/frmMapController.js
@@ -1,5 +1,59 @@
 define({ 
+  onViewCreated(){
+    this.view.init = () => {
+      this.view.cmpSimpleHeader.onClickLeft = () => {
+        this.openMenu();
+      };
 
- //Type your controller code here 
+      this.view.flxCloseMenu.onClick = () => {
+        this.closeMenu();
+      };
 
- });
+      this.view.flxGrey.onClick = () => {
+        this.closeMenu();
+      };
+    };
+  },
+
+  openMenu(){
+    const flxGrey = this.view.flxGrey;
+    var animationArgs = {        
+      duration: 0.5,
+      fillMode: kony.anim.FILL_MODE_FORWARDS,
+      iterationCount: 1
+    };
+
+    this.view.flxMenu.animate(kony.ui.createAnimation({
+      100: {
+        left: 0
+      }
+    }), animationArgs, {
+      animationStart(){
+        flxGrey.isVisible = true;
+      },
+      animationEnd(){
+      }
+    });
+  },
+
+  closeMenu(){
+    const flxGrey = this.view.flxGrey;
+    var animationArgs = {        
+      duration: 0.5,
+      fillMode: kony.anim.FILL_MODE_FORWARDS,
+      iterationCount: 1
+    };
+
+    this.view.flxMenu.animate(kony.ui.createAnimation({
+      100: {
+        left: "-90%"
+      }
+    }), animationArgs, {
+      animationStart(){
+        flxGrey.isVisible = false;
+      },
+      animationEnd(){
+      }
+    });
+  }
+});
