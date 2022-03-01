1e875fe5a530c7a28316020f7f65d0d2d8724642
Mario D'Anna <mario.danna@hcl.com>
2022-03-01 19:47:06 +0100

    Added popup to frmChargingStation

--- controllers/mobile/frmChargingStationController/frmChargingStationController.js
+++ controllers/mobile/frmChargingStationController/frmChargingStationController.js
@@ -1,5 +1,20 @@
 define({ 
 
- //Type your controller code here 
+	onViewCreated(){
+      this.view.init = () => {
+        this.view.flxReserve.onClick = () => {
+          this.view.flxPopupBackground.isVisible = true;
+          this.view.flxPopup.isVisible = true;
+        };
+        this.view.flxPopupBackground.onClick = () => {
+          this.view.flxPopupBackground.isVisible = false;
+          this.view.flxPopup.isVisible = false;
+        };
+        this.view.lblClose.onTouchEnd = () => {
+          this.view.flxPopupBackground.isVisible = false;
+          this.view.flxPopup.isVisible = false;
+        };
+      };
+    }
 
- });
+});
